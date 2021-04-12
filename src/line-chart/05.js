c1756d4a79e8198293951070479dc097e304cdfb
Felipe Chernicharo <string7dev@gmail.com>
2021-04-12 19:47:05 -0300

    dots via scales

--- src/line-chart/05.js
+++ src/line-chart/05.js
@@ -6,8 +6,8 @@ window.addEventListener("resize", (e) => {
 const data = [
 	{ timestamp: 1617246000, value: 1219237.18 }, // 01/04
 	{ timestamp: 1617591600, value: 1175532.41 }, // 05/04
-	{ timestamp: 1617678000, value: 1033334.91 }, // 06/04
-	{ timestamp: 1617764400, value: 1004713.67 }, // 07/04
+	// { timestamp: 1617678000, value: 1033334.91 }, // 06/04
+	// { timestamp: 1617764400, value: 1004713.67 }, // 07/04
 	{ timestamp: 1616986800, value: 960286.28 }, // 29/03
 	{ timestamp: 1617159600, value: 942012.96 }, // 31/03
 	{ timestamp: 1617073200, value: 910421.99 }, // 30/03
@@ -20,24 +20,36 @@ const data = [
 	{ timestamp: 1617505200, value: 663821.39 }, // 04/04
 ].sort((a, b) => a.timestamp - b.timestamp);
 
+function updateChart() {
+	document.querySelector("svg").remove();
+	drawChart();
+}
+
 function drawChart() {
 	const width = window.innerWidth - 100;
 	const height = window.innerHeight - 200;
 	const datesArr = data.map((item) => new Date(item.timestamp * 1000));
+	const valuesArr = data.map((item) => item.value);
+	const maxValue = Math.max(...data.map((o) => o.value));
+	console.log(maxValue);
 
 	const svgCanvas = createSvgCanvas();
 	const tickTextWidth = 60;
-	const tickTextHeight = 30;
+	const tickTextHeight = 40;
 
 	const myTimeScale = d3
 		.scaleLinear()
 		.domain([datesArr[0], datesArr[datesArr.length - 1]])
-		.range([30, width - 60]);
+		.range([70, width - 60]);
 
-	let dateFormat = d3.timeFormat("%d/%m");
+	const myValuesScale = d3
+		.scaleLinear()
+		.domain([maxValue, 0])
+		.range([30, height - 60]);
 
 	const xAxis = createXAxis();
-	appendXAxis();
+	const yAxis = createYAxis();
+	appendAxis();
 
 	function createSvgCanvas() {
 		return d3
@@ -48,20 +60,30 @@ function drawChart() {
 			.style("border", "1px solid");
 	}
 	function createXAxis() {
+		let dateFormat = d3.timeFormat("%d/%m");
+
 		return d3
 			.axisBottom(myTimeScale)
 			.ticks(width / tickTextWidth)
-			.tickFormat(dateFormat);
+			.tickFormat(dateFormat)
+			.tickSizeOuter(0);
 	}
-	function appendXAxis() {
+	function createYAxis() {
+		return d3
+			.axisLeft(myValuesScale)
+			.ticks(height / tickTextHeight)
+			.tickSizeOuter(0);
+	}
+
+	function appendAxis() {
 		svgCanvas
 			.append("g")
 			.call(xAxis)
-			.attr("transform", `translate(10, ${height - 50})`);
-	}
-}
+			.attr("transform", `translate(${0}, ${height - 50})`);
 
-function updateChart() {
-	document.querySelector("svg").remove();
-	drawChart();
+		svgCanvas
+			.append("g")
+			.call(yAxis)
+			.attr("transform", `translate(${70}, ${10})`);
+	}
 }
---
+++ src/line-chart/06.js
@@ -0,0 +1,96 @@
+window.addEventListener("load", () => drawChart());
+
+window.addEventListener("resize", (e) => {
+	updateChart();
+});
+const data = [
+	{ timestamp: 1617246000, value: 1219237.18 }, // 01/04
+	{ timestamp: 1617591600, value: 1175532.41 }, // 05/04
+	// { timestamp: 1617678000, value: 1033334.91 }, // 06/04
+	// { timestamp: 1617764400, value: 1004713.67 }, // 07/04
+	{ timestamp: 1616986800, value: 960286.28 }, // 29/03
+	{ timestamp: 1617159600, value: 942012.96 }, // 31/03
+	{ timestamp: 1617073200, value: 910421.99 }, // 30/03
+	{ timestamp: 1616641200, value: 889714.14 }, // 25/04
+	{ timestamp: 1616900400, value: 583136.65 }, // 28/03
+	{ timestamp: 1616727600, value: 874530.75 }, // 26/03
+	{ timestamp: 1617418800, value: 830735.09 }, // 03/04
+	{ timestamp: 1617332400, value: 768867.64 }, // 02/04
+	{ timestamp: 1616814000, value: 736122.85 }, // 27/03
+	{ timestamp: 1617505200, value: 663821.39 }, // 04/04
+].sort((a, b) => a.timestamp - b.timestamp);
+
+function updateChart() {
+	document.querySelector("svg").remove();
+	drawChart();
+}
+
+function drawChart() {
+	const width = window.innerWidth - 100;
+	const height = window.innerHeight - 200;
+	const margin = { top: 50, right: 60, bottom: 50, left: 70 };
+
+	const datesArr = data.map((item) => new Date(item.timestamp * 1000));
+	const valuesArr = data.map((item) => item.value);
+	const maxValue = Math.max(...data.map((o) => o.value));
+
+	const svgCanvas = createSvgCanvas();
+
+	const tickTextWidth = 60;
+	const tickTextHeight = 40;
+
+	const myTimeScale = d3
+		.scaleLinear()
+		.domain([datesArr[0], datesArr[datesArr.length - 1]])
+		.range([margin.left, width - margin.right]);
+
+	const myValuesScale = d3
+		.scaleLinear()
+		.domain([maxValue, 0])
+		.range([margin.top, height - margin.bottom]);
+
+	const xAxis = createXAxis();
+	const yAxis = createYAxis();
+	appendAxis();
+
+	function createSvgCanvas() {
+		return d3
+			.select("#container")
+			.append("svg")
+			.attr("width", width)
+			.attr("height", height)
+			.style("border", "1px solid");
+	}
+	function createXAxis() {
+		let dateFormat = d3.timeFormat("%d/%m");
+
+		return d3
+			.axisBottom(myTimeScale)
+			.ticks(width / tickTextWidth)
+			.tickFormat(dateFormat)
+			.tickSizeOuter(10);
+	}
+
+	function createYAxis() {
+		return d3
+			.axisLeft(myValuesScale)
+			.ticks(height / tickTextHeight)
+			.tickSize(margin.left + margin.right - width)
+			.tickSizeOuter(0);
+	}
+
+	function appendAxis() {
+		svgCanvas
+			.append("g")
+			.call(xAxis)
+			.attr("transform", `translate(${0}, ${height - margin.bottom})`);
+
+		svgCanvas
+			.append("g")
+			.call(yAxis)
+			.attr("class", "y-axis")
+			.attr("transform", `translate(${margin.left}, ${0})`);
+
+		svgCanvas.selectAll(".y-axis .tick line").attr("opacity", 0.3);
+	}
+}
